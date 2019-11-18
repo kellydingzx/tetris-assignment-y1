b84b2cdc167c476ac563da64c9ceeab342da2f7a
丁子轩 <dingzixuan@dingzixuandeMacBook-Air.local>
2019-11-18 15:11:12 +0000

    test 1

--- player.py
+++ player.py
@@ -1,12 +1,195 @@
-from board import Direction, Rotation
+from board import Direction, Rotation, Shape
 from random import Random
+import numpy as np
+import statistics
 
 
 class Player:
+    def apply_moves(self, board, moves):
+        # apply the elements in moves to the board.
+        for move in moves:
+            if isinstance(move, Direction):
+                landed = board.move(move)
+            elif isinstance(move, Rotation):
+                landed = board.rotate(move)
+            if landed:
+                return True
+        return False
+    
+    def test_drop(self,movebox,dest):
+        move_list = []
+        if not self.apply_moves(movebox, move_list):
+            num_falling = len(movebox.falling.cells)
+            num_fallen = len(movebox.cells)
+        #find out the representitive x-coordinate of the block
+        if not self.apply_moves(movebox, move_list):
+            x_rep = 10
+            for (x,y) in movebox.falling.cells:
+                if x < x_rep:
+                    x_rep = x
+        #move the block horizontally
+        moves = dest - x_rep
+        if moves < 0:
+            moves = -moves
+            for i in range(moves):
+                move_list.append(Direction.Left)
+        else:
+            for i in range(moves):
+                move_list.append(Direction.Right)
+        if not self.apply_moves(movebox, move_list):
+            movebox.move(Direction.Drop)
+        result = self.find_lowest(movebox,num_falling,num_fallen,dest)
+        return result
+    
+    def land_height(self,movebox,dest):
+        height_list = [0,0,0,0,0,0,0,0,0,0]
+        for x in range(movebox.width):
+            for y in range(movebox.height):
+                if(x,y) in movebox.cells:
+                    real_height = movebox.height - y
+                    if real_height > height_list[x]:
+                        height_list[x] = real_height
+        landing_height = height_list[dest]
+        return landing_height
+    
+    def create_binary(self,movebox):
+        board_binary = np.zeros((24,10))
+        for x in range(movebox.width):
+            for y in range(movebox.height):
+                if(x,y) in movebox.cells:
+                    board_binary[y][x] = 1
+        return board_binary
+    
+    def col_transations(self,movebox):
+        board_binary = self.create_binary(movebox)
+        colTran_list = [] 
+        for m in range(movebox.height):
+            colTran = 0
+            for n in range(movebox.width-1):
+                if board_binary[m][n] != board_binary[m][n+1]:
+                    colTran += 1
+            colTran_list.append(colTran)
+        return sum(colTran_list)
+    
+    def row_transitions(self,movebox):
+        board_binary = self.create_binary(movebox)
+        rowTran_list = []
+        for n in range(movebox.width):
+            rowTran = 0
+            for m in range(movebox.height - 1):
+                if board_binary[m][n] != board_binary[m+1][n]:
+                    rowTran += 1
+            rowTran_list.append(rowTran)
+        return sum(rowTran_list)
+               
+    def find_buried(self,movebox):
+        board_binary = self.create_binary(movebox)
+        buried_list = [] 
+        for n in range(movebox.width):
+            holes = None
+            for m in range(movebox.height):
+                if holes == None and board_binary[m][n] == 1 :
+                    holes = 0
+                if holes != None and board_binary[m][n] == 0:
+                    holes += 1
+            if holes != None:
+                buried_list.append(holes)
+        return sum(buried_list)
+            
+    def find_wells(self,movebox):
+        board_binary = self.create_binary(movebox)
+        well_list = [] 
+        sum_n = [0,1,3,6,10,15,21,28,36,45,55]
+        for m in range(movebox.height):
+            well = 0
+            sum_col = 0
+            for n in range(movebox.width):
+                if board_binary[m][n] == 0:
+                    if (n-1<0 or board_binary[m][n-1] == 1) and (n+1 >= 10 or board_binary[m][n+1] == 1):
+                        well += 1
+                    else:
+                        sum_col += sum_n[well]
+                        wells = 0
+            well_list.append(sum_col)
+        return sum(well_list)
+     
+    def find_elimination(self,movebox,num_fallen,num_falling):
+        num_after = len(movebox.cells)
+        lines_cancelled = (num_fallen + num_falling - num_after)//10
+        block_donated = lines_cancelled*10 - (num_fallen-num_after-num_falling)
+        eroded = lines_cancelled * block_donated  
+        return eroded
+    
+    def find_lowest(self,movebox,num_falling,num_fallen,dest):
+        landing_height = self.land_height(movebox,dest)
+        colTran_sum = self.col_transations(movebox)
+        rowTran_sum = self.row_transitions(movebox)
+        buried_sum = self.find_buried(movebox)
+        well_sum = self.find_wells(movebox)
+        eroded = self.find_elimination(movebox,num_fallen,num_falling)
+        # evaluation
+        value = (-4.500158825082766 * landing_height) + 3.4181268101392694 * eroded - 3.2178882868487753 * rowTran_sum - 9.348695305445199 *colTran_sum - 7.899265427351652 * buried_sum - 3.3855972247263626 * well_sum
+        return value
+    
+    def m_permutations(self,board):
+        sandbox = board.clone()
+        rotate_list = []
+        if sandbox.falling.shape == Shape.I:
+            rotate_list = [0,1]  
+        elif sandbox.falling.shape == Shape.J:
+            rotate_list = [0,1,2,3]
+        elif sandbox.falling.shape == Shape.L:
+            rotate_list = [0,1,2,3]
+        elif sandbox.falling.shape == Shape.O:
+            rotate_list = [0]
+        elif sandbox.falling.shape == Shape.T:
+            rotate_list = [0,1,2,3,4]
+        elif sandbox.falling.shape == Shape.S:
+            rotate_list = [0,1,2]
+        elif sandbox.falling.shape == Shape.Z:
+            rotate_list = [0,1,2]
+        move_dest_list = [0,1,2,3,4,5,6,7,8,9]
+        height_achieved = []
+        for rot in rotate_list:
+            testbox = sandbox.clone()
+            for i in range(rot):
+                testbox.rotate(Rotation.Clockwise)
+            for dest in move_dest_list:
+                movebox = testbox.clone()
+                height = self.test_drop(movebox,dest)
+                height_achieved.append([rot,dest,height]) 
+        height_list = []
+        for lst in height_achieved:
+            height_list.append(lst[2])  
+        height_min = max(height_list)
+        num = height_list.count(height_min)
+        for lst in height_achieved:
+            if lst[2] == height_min:
+                return [lst[0],lst[1]]
+        
     def choose_action(self, board):
-        raise NotImplementedError
-
-
+        move = self.m_permutations(board)
+        #implement
+        list_actions = []
+        for i in range(move[0]):
+            list_actions.append(Rotation.Clockwise)
+        x_rep = 10
+        for (x,y) in board.falling.cells:
+            if x < x_rep:
+                x_rep = x
+        moves = move[1] - x_rep
+        if moves < 0:
+            moves = -moves
+            for i in range(moves):
+                list_actions.append(Direction.Left)
+        else:
+            for i in range(moves):
+                list_actions.append(Direction.Right)
+        list_actions.append(Direction.Drop)
+        if self.apply_moves(board,list_actions):
+            return list_actions
+        
+        
 class RandomPlayer(Player):
     def __init__(self, seed=None):
         self.random = Random(seed)
@@ -21,4 +204,4 @@ class RandomPlayer(Player):
         ])
 
 
-SelectedPlayer = RandomPlayer
+SelectedPlayer = Player
